a4cdd9195ac1ac282abfbf637653cb9f1fbee43a
Shravn Debbad <33937523+sdebbad@users.noreply.github.com>
2018-02-12 23:32:48 -0800

    Update ERC223 token.sol

--- ERC223 token.sol
+++ ERC223 token.sol
@@ -6,7 +6,20 @@ contract ERC223Interface {
     function transfer(address to, uint value, bytes data);
     event Transfer(address indexed from, address indexed to, uint value, bytes data);
 }
-import './ERC223_receiving_contract.sol';
+ /**
+ * @title Contract that will work with ERC223 tokens.
+ */
+ 
+contract ERC223ReceivingContract { 
+/**
+ * @dev Standard ERC223 function that will handle incoming token transfers.
+ *
+ * @param _from  Token sender address.
+ * @param _value Amount of tokens.
+ * @param _data  Transaction metadata.
+ */
+    function tokenFallback(address _from, uint _value, bytes _data);
+}
 import '././SafeMath.sol';
 
 /**
