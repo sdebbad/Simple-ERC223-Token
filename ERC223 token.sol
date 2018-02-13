6c90c9e463ccb86b75e65f2a68a0dd37b4d0f0b5
Shravn Debbad <33937523+sdebbad@users.noreply.github.com>
2018-02-12 23:31:45 -0800

    Update ERC223 token.sol

--- ERC223 token.sol
+++ ERC223 token.sol
@@ -1,6 +1,11 @@
 pragma solidity ^0.4.11;
-
-import './ERC223_interface.sol';
+contract ERC223Interface {
+    uint public totalSupply;
+    function balanceOf(address who) constant returns (uint);
+    function transfer(address to, uint value);
+    function transfer(address to, uint value, bytes data);
+    event Transfer(address indexed from, address indexed to, uint value, bytes data);
+}
 import './ERC223_receiving_contract.sol';
 import '././SafeMath.sol';
 
